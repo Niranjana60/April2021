65c0112c1300e325511b9b8a2e1b5648c8527983
Niranjana60 <niruradh61@gmail.com>
2021-04-13 08:45:16 +0100

    Login User

---
+++ April2021/Tests/loginuser.cs
@@ -0,0 +1,46 @@
+﻿using April2021.Page;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace April2021
+{
+    class loginuser
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("helloworld");
+            // launch turnup portal
+
+            IWebDriver driver = new ChromeDriver();
+
+            //page objects for login
+            Loginpage loginobj = new Loginpage();
+            loginobj.loginsteps(driver);
+
+            //page objects for homepage
+            Homepage homeobj = new Homepage();
+            homeobj.navigateToTM(driver);
+
+            //page objects for TMPage
+            TMPage tmobj = new TMPage();
+            tmobj.createTM(driver);
+            tmobj.editTM(driver);
+            tmobj.deleteTM(driver);
+
+            //close driver
+            driver.Close();
+
+
+
+        }
+    }
+}
+
+
+
+
+
